4d70cb7a36db56159568a7df28bc1c496caa0ea4
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-02 17:33:13 -0400

    update

---
+++ image-upload/page-display.php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+    <title>Display</title>
+    <style>
+    body {
+        background-color: lightblue;
+    }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>Children Info From The Database</h1>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table width="50%" border="1" cellpadding="5" cellspacing="5">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <?php
+        $connection=mysqli_connect("localhost","u376224787_S7LG6","3k[64RCn~P");
+$db=mysqli_select_db($connection,'u376224787_2ZJdV');
+$query="SELECT * FROM children";
+$query_run=mysqli_query($connection,$query);
+while($row=mysqli_fetch_array($query_run))
+{
+  ?>
+                <tr>
+                    <td><?php echo '<img src="data:image;base64,'.base64_encode($row['image']).'" alt="Image" style="width: 100px; height: 100px;" >'; ?>
+                    </td>
+                    <td><?php echo $row['name']; ?> </td>
+                    <td><?php echo $row['age']; ?> </td>
+                    <td><?php echo $row['city']; ?> </td>
+                </tr>
+                <?php
+}
+?>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
---
+++ image-upload/page-upload.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+    <title>Upload Image</title>
+    <style>
+    body {
+        background-color: yellow;
+    }
+
+    input {
+        width: 50%;
+        height: 5%;
+        border: 1px;
+        border-radius: 5px;
+        padding: 8px 15px 8px 15px;
+        margin: 10px 0px 15px 0px;
+        box-shadow: 1px 1px 2px 1px grey;
+        font-weight: bold;
+    }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>Upload A Child Info Into Our Database</h1>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <label>Age (Number):</label><br>
+            <input type="text" name="age" /><br>
+            <label>Name:</label><br>
+            <input type="text" name="name" /><br>
+            <label>City:</label><br>
+            <input type="text" name="city" /><br>
+            <label>Choose an Profile Pic:</label><br>
+            <input type="file" name="image" id="image" /><br>
+            <input type="submit" name="upload" value="Upload Image/Data" /><br>
+        </form>
+    </center>
+</body>
+
+</html>
+
+<?php
+$connection=mysqli_connect("localhost","u376224787_S7LG6","3k[64RCn~P");
+$db=mysqli_select_db($connection,'u376224787_2ZJdV');
+
+if(isset($_POST['upload']))
+{
+  $file=addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+  $age=$_POST['age'];
+  $name=$_POST['name'];
+  $city=$_POST['city'];
+  $query="INSERT INTO children (age,name,city,image) VALUES ('$age','$name','$city','$file')";
+  $query_run=mysqli_query($connection,$query);
+  if($query_run)
+  {
+    echo'<script type="text/javascript">alert("Uploaded!")</script>';
+  }
+  else{
+    echo'<script type="text/javascript">alert("Not uploaded!")</script>';
+  }
+}
+?>
