e15b2581349924dcc05b01a69dbee9b1bc165cf6
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-09 23:44:42 -0400

    update

--- plugins/setting.php
+++ plugins/setting.php
@@ -6,14 +6,22 @@
  class WordCountAndTimePlugin{
    function __construct(){
     add_action('admin_menu',array($this,'adminPage'));
+    add_action('admin_init',array($this,'settings'));
+   }
+
+   function settings(){
+     add_settings_field('wcp_location','Display Location',array($this,'locationHTML'),'word-count-settings-page','wcp_first_section');
+     register_setting('wordcountplugin','wcp_location',array('sanitize_callback'=>'sanitize_text_field','default'=>'0'));
    }
 
    function adminPage() {
-    add_options-page('Word Count Settings','Word Count','manage_options','word-count-settings-page','ourSettingsPageHTML');
+    add_options-page('Word Count Settings','Word Count','manage_options','word-count-settings-page',array($this,'ourHTML'));
 }
 
-function ourSettingsPageHTML(){?>
-wdwqadwqdwqdwqadwq
+function ourHTML(){?>
+<div class="wrap">
+    <h1>Word Count Settings</h1>
+</div>
 <?php }
  }
 
