085f140befaa2c2559d524672988361724f4d32a
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-11 20:40:06 -0400

    update

--- plugins/setting.php
+++ plugins/setting.php
@@ -11,16 +11,24 @@
 
    function settings(){
      add_settings_section('wcp_first_section',null,null,'word-count-settings-page');
+
      add_settings_field('wcp_location','Display Location',array($this,'locationHTML'),'word-count-settings-page','wcp_first_section');
      register_setting('wordcountplugin','wcp_location',array('sanitize_callback'=>'sanitize_text_field','default'=>'0'));
-   }
 
-   function locationHTML(){?>
+     add_settings_field('wcp_headline','Headline Text',array($this,'headlineHTML'),'word-count-settings-page','wcp_first_section');
+     register_setting('wordcountplugin','wcp_headline',array('sanitize_callback'=>'sanitize_text_field','default'=>'Post Statistics'));
+   }
+   
+   function headlineHTML(){?>
+   <input type="text" name="wcp-headline" value="<?php echo esc_attr(get_option('wcp-headline')) ?>" >
+  <?php}
+
+   function locationHTML(){ ?>
 <select name="wcp_location">
-    <option value="0">Beginning of post</option>
-    <option value="1">End of post</option>
+    <option value="0" <?php selected(get_option('wcp-location'),'0') ?>>Beginning of post</option>
+    <option value="1" <?php selected(get_option('wcp-location'),'1') ?>>End of post</option>
 </select>
-<?php}
+<?php }
 
    function adminPage() {
     add_options-page('Word Count Settings','Word Count','manage_options','word-count-settings-page',array($this,'ourHTML'));
