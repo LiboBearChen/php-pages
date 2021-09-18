0b575038e839a0b80d6834d3b9885a00ebb57040
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-17 20:10:05 -0400

    update

---
+++ plugins/AreYouPayingAttention/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Plugin Name: Are You Paying Attention Quiz
+*/
+
+if (!defined('ABSPATH')) exit;
+
+class AreYouPayingAttention
+{
+    function __construct()
+    {
+        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    }
+
+    function adminAssets()
+    {
+        wp_enqueue_script('ournewBlocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks'));
+    }
+}
+
+$areYouPayingAttention = new AreYouPayingAttention();
---
+++ plugins/AreYouPayingAttention/test.js
@@ -0,0 +1,7 @@
+wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
+  title: "Are You Paying Attention?",
+  icon: "smiley",
+  category: "common",
+  edit: function () {},
+  save: function () {},
+});
