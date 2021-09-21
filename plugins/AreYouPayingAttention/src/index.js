b5609bf42df8bf1926778319f1b1ac0dc63eb6c7
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-21 19:36:30 -0400

    update

--- plugins/AreYouPayingAttention/src/index.js
+++ plugins/AreYouPayingAttention/src/index.js
@@ -10,14 +10,32 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     function updateSkyColor(event) {
       props.setAttributes({ skyColor: event.target.value });
     }
+    function updateGrassColor(event) {
+      props.setAttributes({ grassColor: event.target.value });
+    }
     return (
       <div>
-        <input type="text" placeholder="sky color" onChange={updateSkyColor} />
-        <input type="text" placeholder="grass color" />
+        <input
+          type="text"
+          placeholder="sky color"
+          value={props.attributes.skyColor}
+          onChange={updateSkyColor}
+        />
+        <input
+          type="text"
+          placeholder="grass color"
+          value={props.attributes.grassColor}
+          onChange={updateGrassColor}
+        />
       </div>
     );
   },
-  save: function () {
-    return wp.element.createElement("h1", null, "This is the frontend.");
+  save: function (props) {
+    return (
+      <p>
+        Today the sky is {props.attributes.skyColor} and the grass is{" "}
+        {props.attributes.grassColor}.
+      </p>
+    );
   },
 });
