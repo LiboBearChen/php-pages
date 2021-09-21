c51ce5301b8237a7275b664956a70413317df61a
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-09-20 21:39:30 -0400

    update

--- plugins/AreYouPayingAttention/src/index.js
+++ plugins/AreYouPayingAttention/src/index.js
@@ -2,8 +2,20 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
   title: "Are You Paying Attention?",
   icon: "smiley",
   category: "common",
-  edit: function () {
-    return <h1>efwfewfewfwe</h1>;
+  attributes: {
+    skyColor: { type: "string" },
+    grassColor: { type: "string" },
+  },
+  edit: function (props) {
+    function updateSkyColor(event) {
+      props.setAttributes({ skyColor: event.target.value });
+    }
+    return (
+      <div>
+        <input type="text" placeholder="sky color" onChange={updateSkyColor} />
+        <input type="text" placeholder="grass color" />
+      </div>
+    );
   },
   save: function () {
     return wp.element.createElement("h1", null, "This is the frontend.");
